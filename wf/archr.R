7b15f46186dc9a0c35f7c2073f31258ce045191d
Joshua Barnett <joshuab@atlasxomics.com>
2023-10-16 11:49:07 -0400

    Scripts to add cluster data to tissue_position

--- wf/archr.R
+++ wf/archr.R
@@ -7,3 +7,10 @@ positions_file <- args[2]
 archrproject <- args[3]
 
 proj <- loadArchRProject(archrproject)
+
+col <- proj@cellColData@listData[["Clusters"]]
+col2 <- proj@cellColData@rownames
+unlist(col)
+unlist(col2)
+combine <- array(c(col2, col), dim = c(length(col), 2, 1))
+write.csv(combine, "barcodes_clusters.csv", row.names=FALSE)
