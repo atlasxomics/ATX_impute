ede1f10349d98e670d1f49343a6c919ff9f9ddd1
Joshua Barnett <joshuab@atlasxomics.com>
2023-12-08 22:10:57 -0500

    Make cleaning_metric 1-index

--- wf/impute.py
+++ wf/impute.py
@@ -353,8 +353,8 @@ if __name__ == '__main__':
     missing_lanes['row'] = missing_rows
     missing_lanes['col'] = missing_cols
 
-    metrics_output['col'] = ','.join([str(i) for i in missing_cols])
-    metrics_output['row'] = ','.join([str(i) for i in missing_cols])
+    metrics_output['col'] = ','.join([str(i+1) for i in missing_cols])
+    metrics_output['row'] = ','.join([str(i+1) for i in missing_cols])
     fragments_path = sys.argv[4]
     position_path = sys.argv[5]
 
